6cddb3b4b1fb0fa4750bbf45a375116a3703b72e
AustinReese1998 <reesau01@luther.edu>
2020-05-12 13:31:11 -0500

    disable debug

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def index():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug = True)
+    app.run(host='0.0.0.0', port=port)
